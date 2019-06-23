1134fe213a59c8f257f6262076aaab0dbdc0e130
Mustafa <mjebbar17@gmail.com>
2019-06-23 11:25:16 +0100

    in dev - in vote logic

--- src/screens/main/game-brain/in-vote/index.js
+++ src/screens/main/game-brain/in-vote/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import { View, Text, BackHandler, ToastAndroid, } from 'react-native'
+import {View, Text, BackHandler, ToastAndroid, ScrollView, FlatList,} from 'react-native'
 import styles from '../../../../styles/global';
 import { connect } from 'react-redux';
-import { updateGameData, setGameDisconnect, updatePlayersData, setPlayersDisconnect } from '../../../../redux/actions/gameActions';
 import { NavigationEvents } from 'react-navigation';
-import PlayersList from '../../../../components/playersList';
-import ReadyButton from '../../../../components/playerReadyButton';
 
 class InVote extends React.Component {
     static navigationOptions = { header: null }
@@ -23,7 +20,45 @@ class InVote extends React.Component {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
     }
 
+    voteForPlayer = player => {
+        const { gameDoc } = this.props;
+        //TODO: add needed data to VotingFor
+        gameDoc.ref.collections('players').doc(currentPlayer.email).update({votingFor: player.displayName})
+    }
+
+    handleVotingComplete = () => {
+
+        const { inGamePlayers } = this.props;
+        const votingResults =  inGamePlayers.reduce(function(map, player){
+            if(!(player.displayName in map)) {
+                map[player.displayName] = 1;
+            }else{
+                map[player.displayName]++;
+            }
+            return map;
+        }, {});
+
+        const max = Math.max(Object.values(votingResults));
+        const highestVotedForPlayers = [];
+        Object.keys(votingResults).forEach( player => {
+            if(votingResults[player] === max) highestVotedForPlayers.push(player);
+        });
+
+        if(highestVotedForPlayers.length > 1){
+            //DRAW
+
+        } else {
+            //END VOTES
+        }
+    }
+
     render() {
+
+        const { players, allPlayersHaveVoted } = this.props;
+
+        if(allPlayersHaveVoted && currentPlayer.isAdmin){
+            this.handleVotingComplete();
+        }
 
         return (
            <View style={styles.page}>
@@ -34,6 +69,17 @@ class InVote extends React.Component {
                />
 
                <Text> InVote </Text>
+
+               <ScrollView>
+                   <FlatList
+                      data={players}
+                      renderItem={({player}) =>
+                         <Button title={player.displayName} onPress={ () => {
+                             this.voteForPlayer(player)}
+                         }/>
+                      }
+                   />
+               </ScrollView>
            </View>
         )
     }
@@ -41,7 +87,13 @@ class InVote extends React.Component {
 
 
 const mapStateToProps = state => ({
-    user: state.user.data
+    user: state.user.data,
+    gameDoc: state.game.gameDoc,
+    players: state.game.playersData,
+    inGamePlayers: state.game.playersData.filter( player => !player.isOut)
+    allPlayersHaveVoted: state.game.playersData.filter( player => !player.isOut).reduce( (allVoted ,player) => (allReady && !!player.votedFor), true),
+    currentPlayer: state.game.playersData.find( player => player.displayName === state.user.data.displayName),
+
 })
 
 const mapDispatchToProps = dispatch => ({})
