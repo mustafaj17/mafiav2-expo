2007b2903cb2a1a67a899345d7bb0d47bdccc7c8
Mahmoud Hassan <mahmoud.hassan@8x8.com>
2020-01-05 21:21:33 +0000

    checktypemessage

--- src/components/checkTypeMessage/index.js
+++ src/components/checkTypeMessage/index.js
@@ -18,96 +18,89 @@ const CheckTypeMessage = ({userSeenType, hideMessage}) => {
   };
 
   return (
-    <View style={{width: '100%'}}>
-      <Modal visible transparent animationType="slide">
-        <View style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'absolute',
-          height: '100%',
-          width: '100%',
-          backgroundColor: 'rgba(77,77,77, 0.99)',
-        }}>
-          <View style={{
-            height: '100%',
-            width: '100%',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            paddingTop: 40,
-            paddingBottom: 40,
-          }}>
-            <Text size='large' type='bold'> LET'S PLAY</Text>
-            <View style={{padding: 10}}>
-              <Text>CHECK IF YOU ARE A <Text color='red'>MAFIA</Text> OR <Text color='blue'>CIVILIAN</Text> BY PRESSING THE SHOW TYPE BUTTON IN THE BOTTOM RIGHT CORNER</Text>
-            </View>
+    <Modal visible transparent animationType="slide">
+      <View style={{
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'rgba(77,77,77, 0.99)',
+        paddingTop: 40,
+        paddingBottom: 40,
+      }}>
 
-            <View style={{width: '100%'}}>
-              <LinearGradient
-                start={{x: 0, y: -0.5}} end={{x: 0, y: 1}}
-                colors={['#811C24', '#DB1C24']}
-                style={{
-                  width: '100%',
-                  display: 'flex',
-                  paddingTop: 10,
-                  paddingBottom: 10,
-                  justifyContent: 'center',
-                  alignItems: 'center' ,
-                  position: 'relative',
-                  flexDirection: 'row'
-                }}>
-                <Image source={mafiaIcon}
-                       resizeMode='contain'
-                       style= {{
-                         height: 150,
-                         width: 150,
-                       }}/>
-                <View style={{width: '60%'}}>
-                  <Text size='xxsmall' style={{marginBottom:10}}>
-                    IF YOU ARE A MAFIA YOU WILL BE ABLE TO SEE OTHER MAFIAS - YOU MUST TRICK EVERYONE INTO BELIEVING YOU ARE A CIVILIAN - WORK WITH OTHER MAFIAS TO ELIMINATE ALL THE CIVILIANS
-                  </Text>
-                </View>
-              </LinearGradient>
-
-              <LinearGradient
-                start={{x: 0, y: -0.5}} end={{x: 0, y: 1}}
-                colors={['#0000FF', '#000054']}
-                style={{
-                  width: '100%',
-                  display: 'flex',
-                  paddingTop: 10,
-                  paddingBottom: 10,
-                  justifyContent: 'center',
-                  alignItems: 'center' ,
-                  position: 'relative',
-                  flexDirection: 'row'
-                }}>
-                <Image source={civIcon}
-                       resizeMode='contain'
-                       style= {{
-                         height: 150,
-                         width: 150,
-                       }}/>
-                <View style={{width: '60%'}}>
-                  <Text size='xxsmall' style={{marginBottom:10}}>
-                    IF YOU ARE A CIVILIAN YOU MUST TRY TO FIND AND ELIMINATE THE MAFIAS - BE SMART AND ATTENTIVE AND SEE WHO IS ACTING STRANGELY
-                  </Text>
-                </View>
-              </LinearGradient>
-            </View>
-
-            <View style={{display: 'flex', flexDirection: 'row', width: '99%', justifyContent: 'space-around', alignItems: 'center'}}>
-              <Button onPress={handleDoNotShowMessage} margin={2}>
-                <Text size='xsmall' color='white' width='100%'>DON'T SHOW AGAIN</Text>
-              </Button>
-              <Button secondary onPress={hideMessage} width={100} margin={2}>
-                <Text size='xsmall'>OK</Text>
-              </Button>
-            </View>
-          </View>
+        <Text size='large' type='bold'> LET'S PLAY</Text>
+        <View style={{padding: 10}}>
+          <Text>CHECK IF YOU ARE A <Text color='red'>MAFIA</Text> OR <Text color='blue'>CIVILIAN</Text> BY PRESSING THE SHOW TYPE BUTTON IN THE BOTTOM RIGHT CORNER</Text>
         </View>
-      </Modal>
-    </View>
+
+        <View style={{width: '100%'}}>
+          <LinearGradient
+            start={{x: 0, y: -0.5}} end={{x: 0, y: 1}}
+            colors={['#811C24', '#DB1C24']}
+            style={{
+              width: '100%',
+              display: 'flex',
+              paddingTop: 10,
+              paddingBottom: 10,
+              justifyContent: 'center',
+              alignItems: 'center' ,
+              position: 'relative',
+              flexDirection: 'row'
+            }}>
+            <Image source={mafiaIcon}
+                   resizeMode='contain'
+                   style= {{
+                     height: 150,
+                     width: 150,
+                   }}/>
+            <View style={{width: '60%'}}>
+              <Text size='xxsmall' style={{marginBottom:10}}>
+                IF YOU ARE A MAFIA YOU WILL BE ABLE TO SEE OTHER MAFIAS - YOU MUST TRICK EVERYONE INTO BELIEVING YOU ARE A CIVILIAN - WORK WITH OTHER MAFIAS TO ELIMINATE ALL THE CIVILIANS
+              </Text>
+            </View>
+          </LinearGradient>
+
+          <LinearGradient
+            start={{x: 0, y: -0.5}} end={{x: 0, y: 1}}
+            colors={['#0000FF', '#000054']}
+            style={{
+              width: '100%',
+              display: 'flex',
+              paddingTop: 10,
+              paddingBottom: 10,
+              justifyContent: 'center',
+              alignItems: 'center' ,
+              position: 'relative',
+              flexDirection: 'row'
+            }}>
+            <Image source={civIcon}
+                   resizeMode='contain'
+                   style= {{
+                     height: 150,
+                     width: 150,
+                   }}/>
+            <View style={{width: '60%'}}>
+              <Text size='xxsmall' style={{marginBottom:10}}>
+                IF YOU ARE A CIVILIAN YOU MUST TRY TO FIND AND ELIMINATE THE MAFIAS - BE SMART AND ATTENTIVE AND SEE WHO IS ACTING STRANGELY
+              </Text>
+            </View>
+          </LinearGradient>
+        </View>
+
+        <View style={{display: 'flex', flexDirection: 'row', width: '99%', justifyContent: 'space-around', alignItems: 'center'}}>
+          <Button onPress={handleDoNotShowMessage} margin={2}>
+            <Text size='xsmall' color='white' width='100%'>DON'T SHOW AGAIN</Text>
+          </Button>
+          <Button secondary onPress={hideMessage} width={100} margin={2}>
+            <Text size='xsmall'>OK</Text>
+          </Button>
+        </View>
+
+      </View>
+    </Modal>
   )};
 
 export default CheckTypeMessage;
