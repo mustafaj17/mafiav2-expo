bb9a44a531869a5701c974b80102490f8e68fa97
Mustafa J <mjebbar@cisco.com>
2020-01-24 14:44:04 +0000

    keyboard view fix

--- src/components/mafiaBackground/index.js
+++ src/components/mafiaBackground/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
-import { View } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 
 const MafiaBackground = props => {
   return (
-    <LinearGradient
-      start={{ x: 0, y: -0.5 }}
-      end={{ x: 0, y: 1 }}
-      colors={['#4d4d4d', '#1a1a1a']}
-      style={{ flex: 1, width: '100%' }}
-      {...props}>
-      {props.children}
-    </LinearGradient>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{flex: 1}}>
+      <LinearGradient
+        start={{ x: 0, y: -0.5 }}
+        end={{ x: 0, y: 1 }}
+        colors={['#4d4d4d', '#1a1a1a']}
+        style={{ flex: 1, width: '100%' }}
+        {...props}>
+        {props.children}
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 };
 
